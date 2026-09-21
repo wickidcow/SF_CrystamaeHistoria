91ad05678011d52dbe57d7d3bb5a8acdeca5f399
wickidcow <jaywago@gmail.com>
2026-09-21 19:26:54 -0400

    Run armor stand animation on entity scheduler

--- src/main/java/io/github/sefiraat/crystamaehistoria/runnables/spells/FloatingHeadAnimation.java
+++ src/main/java/io/github/sefiraat/crystamaehistoria/runnables/spells/FloatingHeadAnimation.java
@@ -1,23 +1,47 @@
 package io.github.sefiraat.crystamaehistoria.runnables.spells;
 
+import io.github.sefiraat.crystamaehistoria.CrystamaeHistoria;
 import io.github.sefiraat.crystamaehistoria.utils.ArmourStandUtils;
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import org.bukkit.entity.ArmorStand;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
-public class FloatingHeadAnimation extends BukkitRunnable {
+public class FloatingHeadAnimation implements Runnable {
+
     public static final double Y_DEVIANCY = 0.2;
     public static final long SPEED = 1;
 
     private final ArmorStand armorStand;
     private final double baseY;
     private boolean directionUp = true;
+    private ScheduledTask task;
 
     @ParametersAreNonnullByDefault
     public FloatingHeadAnimation(ArmorStand armorStand) {
         this.armorStand = armorStand;
-        baseY = armorStand.getLocation().getY();
+        this.baseY = armorStand.getLocation().getY();
+    }
+
+    public void start() {
+        if (task != null) {
+            return;
+        }
+
+        task = armorStand.getScheduler().runAtFixedRate(
+            CrystamaeHistoria.getInstance(),
+            scheduledTask -> run(),
+            () -> task = null,
+            1L,
+            SPEED
+        );
+    }
+
+    public void cancel() {
+        if (task != null) {
+            task.cancel();
+            task = null;
+        }
     }
 
     @Override
