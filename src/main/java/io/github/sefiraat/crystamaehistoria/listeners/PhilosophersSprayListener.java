ac461cdf03ad32a995101fecdd02aeaf83091c1d
wickidcow <jaywago@gmail.com>
2026-09-22 12:07:39 -0400

    Use modern Slimefun storage lookup

--- src/main/java/io/github/sefiraat/crystamaehistoria/listeners/PhilosophersSprayListener.java
+++ src/main/java/io/github/sefiraat/crystamaehistoria/listeners/PhilosophersSprayListener.java
@@ -1,9 +1,9 @@
 package io.github.sefiraat.crystamaehistoria.listeners;
 
+import com.xzavier0722.mc.plugin.slimefun4.storage.util.StorageCacheUtils;
 import io.github.sefiraat.crystamaehistoria.slimefun.items.gadgets.PhilosophersSpray;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.papermc.paper.event.block.BlockFailedDispenseEvent;
-import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,7 +13,7 @@ public class PhilosophersSprayListener implements Listener {
     @EventHandler
     public void onInteract(BlockFailedDispenseEvent e) {
         final Block block = e.getBlock();
-        SlimefunItem item = BlockStorage.check(block);
+        SlimefunItem item = StorageCacheUtils.getSlimefunItem(block.getLocation());
         if (item instanceof PhilosophersSpray) {
             PhilosophersSpray.triggerChange(block);
         }
