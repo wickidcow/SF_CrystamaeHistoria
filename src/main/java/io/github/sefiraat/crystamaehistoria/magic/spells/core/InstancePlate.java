e13d35c842432eee700721027d55a88006a6c1f1
wickidcow <jaywago@gmail.com>
2026-09-22 12:39:01 -0400

    Modernize spell and story text APIs

--- src/main/java/io/github/sefiraat/crystamaehistoria/magic/spells/core/InstancePlate.java
+++ src/main/java/io/github/sefiraat/crystamaehistoria/magic/spells/core/InstancePlate.java
@@ -8,8 +8,9 @@ import io.github.sefiraat.crystamaehistoria.player.PlayerStatistics;
 import io.github.sefiraat.crystamaehistoria.utils.TextUtils;
 import io.github.sefiraat.crystamaehistoria.utils.theme.ThemeType;
 import lombok.Getter;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import lombok.Setter;
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -46,7 +47,7 @@ public class InstancePlate {
             ThemeType.CLICK_INFO.getColor() + "Magic Framework : " + ThemeType.NOTICE.getColor() + magic,
             ThemeType.CLICK_INFO.getColor() + "Stored Crysta : " + ThemeType.NOTICE.getColor() + crysta
         };
-        final ChatColor passiveColor = ThemeType.PASSIVE.getColor();
+        final String passiveColor = ThemeType.PASSIVE.getColor();
         final List<String> finalLore = new ArrayList<>();
         final ItemMeta itemMeta = itemStack.getItemMeta();
 
@@ -57,7 +58,11 @@ public class InstancePlate {
         finalLore.add("");
         finalLore.add(ThemeType.applyThemeToString(ThemeType.CLICK_INFO, ThemeType.CRAFTING.getLoreLine()));
 
-        itemMeta.setLore(finalLore);
+        itemMeta.lore(finalLore.stream()
+            .map(line -> LegacyComponentSerializer.legacySection()
+                .deserialize(line)
+                .decoration(TextDecoration.ITALIC, false))
+            .toList());
         itemStack.setItemMeta(itemMeta);
     }
 
