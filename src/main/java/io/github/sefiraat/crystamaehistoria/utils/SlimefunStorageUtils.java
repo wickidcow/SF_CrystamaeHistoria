15e5dc73202c6b89a97f5e96778c78381881f378
wickidcow <jaywago@gmail.com>
2026-09-22 13:00:01 -0400

    Expand cross-version Slimefun storage helper

--- src/main/java/io/github/sefiraat/crystamaehistoria/utils/SlimefunStorageUtils.java
+++ src/main/java/io/github/sefiraat/crystamaehistoria/utils/SlimefunStorageUtils.java
@@ -2,6 +2,8 @@ package io.github.sefiraat.crystamaehistoria.utils;
 
 import com.xzavier0722.mc.plugin.slimefun4.storage.util.StorageCacheUtils;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
 import org.bukkit.Location;
 
 import javax.annotation.Nullable;
@@ -10,6 +12,33 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public final class SlimefunStorageUtils {
 
     private SlimefunStorageUtils() {
+    }
+
+    @Nullable
+    @ParametersAreNonnullByDefault
+    public static String getData(Location location, String key) {
+        return StorageCacheUtils.getData(location, key);
+    }
+
+    @ParametersAreNonnullByDefault
+    public static void setData(Location location, String key, String value) {
+        StorageCacheUtils.setData(location, key, value);
+    }
+
+    @ParametersAreNonnullByDefault
+    public static void removeData(Location location, String key) {
+        StorageCacheUtils.removeData(location, key);
+    }
+
+    @Nullable
+    @ParametersAreNonnullByDefault
+    public static BlockMenu getMenu(Location location) {
+        return StorageCacheUtils.getMenu(location);
+    }
+
+    @ParametersAreNonnullByDefault
+    public static void removeBlock(Location location) {
+        Slimefun.getDatabaseManager().getBlockDataController().removeBlock(location);
     }
 
     @Nullable
