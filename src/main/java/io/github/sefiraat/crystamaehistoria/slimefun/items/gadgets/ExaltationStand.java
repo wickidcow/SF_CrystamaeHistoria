39a526900e40dfd4e8271657b29139b96160ff5a
wickidcow <jaywago@gmail.com>
2026-09-22 12:25:04 -0400

    Finish modern Slimefun ticker migration

--- src/main/java/io/github/sefiraat/crystamaehistoria/slimefun/items/gadgets/ExaltationStand.java
+++ src/main/java/io/github/sefiraat/crystamaehistoria/slimefun/items/gadgets/ExaltationStand.java
@@ -10,7 +10,7 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.protection.Interaction;
-import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
+import com.xzavier0722.mc.plugin.slimefun4.storage.controller.SlimefunBlockData;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -79,7 +79,7 @@ public class ExaltationStand extends Stand {
     }
 
     @Override
-    public void afterTick(@Nonnull Item item, @Nonnull Block block, @Nonnull SlimefunItem slimefunItem, @Nonnull Config config) {
+    public void afterTick(@Nonnull Item item, @Nonnull Block block, @Nonnull SlimefunItem slimefunItem, @Nonnull SlimefunBlockData config) {
         SlimefunItem itemOnStand = SlimefunItem.getByItem(item.getItemStack());
         if (itemOnStand instanceof ExaltedItem) {
             ExaltedItem exaltedItem = (ExaltedItem) itemOnStand;
