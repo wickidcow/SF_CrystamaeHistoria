a4e2e883c0ecf04558906a49bca640c8130af8eb
wickidcow <jaywago@gmail.com>
2026-09-22 12:12:56 -0400

    Finish Adventure theme string compatibility

--- src/main/java/io/github/sefiraat/crystamaehistoria/utils/theme/ThemeType.java
+++ src/main/java/io/github/sefiraat/crystamaehistoria/utils/theme/ThemeType.java
@@ -166,7 +166,7 @@ public enum ThemeType {
      *
      * @param id        The ID for the new {@link SlimefunItemStack}
      * @param itemStack The vanilla {@link ItemStack} used to base the {@link SlimefunItemStack} on
-     * @param themeType The {@link ThemeType} {@link ChatColor} to apply to the {@link SlimefunItemStack} name
+     * @param themeType The {@link ThemeType} {@link TextColor} to apply to the {@link SlimefunItemStack} name
      * @param name      The name to apply to the {@link SlimefunItemStack}
      * @param lore      The lore lines for the {@link SlimefunItemStack}. Lore is book-ended with empty strings.
      * @return Returns the new {@link SlimefunItemStack}
@@ -179,7 +179,7 @@ public enum ThemeType {
                                                             String name,
                                                             String... lore
     ) {
-        ChatColor passiveColor = ThemeType.PASSIVE.getColor();
+        String passiveColor = ThemeType.PASSIVE.getColor();
         List<String> finalLore = new ArrayList<>();
         finalLore.add("");
         for (String s : lore) {
@@ -212,7 +212,7 @@ public enum ThemeType {
      * Gets an ItemStack with a pre-populated lore and name with themed colors.
      *
      * @param material  The {@link Material} used to base the {@link ItemStack} on
-     * @param themeType The {@link ThemeType} {@link ChatColor} to apply to the {@link ItemStack} name
+     * @param themeType The {@link ThemeType} {@link TextColor} to apply to the {@link ItemStack} name
      * @param name      The name to apply to the {@link ItemStack}
      * @param lore      The lore lines for the {@link ItemStack}. Lore is book-ended with empty strings.
      * @return Returns the new {@link ItemStack}
@@ -220,7 +220,7 @@ public enum ThemeType {
     @Nonnull
     @ParametersAreNonnullByDefault
     public static ItemStack themedItemStack(Material material, ThemeType themeType, String name, String... lore) {
-        ChatColor passiveColor = ThemeType.PASSIVE.getColor();
+        String passiveColor = ThemeType.PASSIVE.getColor();
         List<String> finalLore = new ArrayList<>();
         finalLore.add("");
         for (String s : lore) {
