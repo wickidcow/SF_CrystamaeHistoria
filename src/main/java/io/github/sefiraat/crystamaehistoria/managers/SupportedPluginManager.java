7d502b3618ebe62248b01943961fc7f6c17e11bf
wickidcow <jaywago@gmail.com>
2026-09-22 12:40:42 -0400

    Migrate deprecated Slimefun and entity APIs

--- src/main/java/io/github/sefiraat/crystamaehistoria/managers/SupportedPluginManager.java
+++ src/main/java/io/github/sefiraat/crystamaehistoria/managers/SupportedPluginManager.java
@@ -10,7 +10,7 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.data.persistent.PersistentDataAPI;
 import lombok.Getter;
-import me.mrCookieSlime.Slimefun.api.BlockStorage;
+import com.xzavier0722.mc.plugin.slimefun4.storage.util.StorageCacheUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
@@ -94,8 +94,8 @@ public class SupportedPluginManager {
     @ParametersAreNonnullByDefault
     public boolean isExoticGardenPlant(Block block) {
         return exoticGarden
-            && BlockStorage.hasBlockInfo(block)
-            && BlockStorage.check(block) instanceof BonemealableItem;
+            && StorageCacheUtils.hasSlimefunBlock(block.getLocation())
+            && StorageCacheUtils.getSlimefunItem(block.getLocation()) instanceof BonemealableItem;
     }
 
     /**
@@ -108,8 +108,8 @@ public class SupportedPluginManager {
     @Nullable
     @ParametersAreNonnullByDefault
     public SlimefunItem getExoticGardenPlant(Block block) {
-        if (exoticGarden && BlockStorage.hasBlockInfo(block)) {
-            SlimefunItem slimefunItem = BlockStorage.check(block);
+        if (exoticGarden && StorageCacheUtils.hasSlimefunBlock(block.getLocation())) {
+            SlimefunItem slimefunItem = StorageCacheUtils.getSlimefunItem(block.getLocation());
             if (slimefunItem instanceof BonemealableItem) {
                 return slimefunItem;
             }
