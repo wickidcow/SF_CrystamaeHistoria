e2a573d3952306bf6e62d2ef3cc8c98891b0a03b
wickidcow <jaywago@gmail.com>
2026-09-22 12:10:52 -0400

    Use modern falling block spawn API

--- src/main/java/io/github/sefiraat/crystamaehistoria/magic/spells/tier1/Cascada.java
+++ src/main/java/io/github/sefiraat/crystamaehistoria/magic/spells/tier1/Cascada.java
@@ -13,7 +13,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
-import org.bukkit.entity.Entity;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.EntityType;
 import org.bukkit.util.Vector;
 
@@ -70,7 +70,11 @@ public class Cascada extends Spell {
 
         for (Block block : blocks) {
             final BlockData blockData = block.getBlockData();
-            final Entity entity = block.getWorld().spawnFallingBlock(block.getLocation().add(0.5, 0.5, 0.5), blockData);
+            final FallingBlock entity = block.getWorld().spawn(
+                block.getLocation().add(0.5, 0.5, 0.5),
+                FallingBlock.class,
+                fallingBlock -> fallingBlock.setBlockData(blockData)
+            );
             entity.setVelocity(new Vector(0, 2, 0));
             block.setType(Material.AIR, false);
         }
