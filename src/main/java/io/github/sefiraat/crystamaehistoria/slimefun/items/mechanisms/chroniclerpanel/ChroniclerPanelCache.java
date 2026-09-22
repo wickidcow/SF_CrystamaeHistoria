d2ac123b491d6b6efeb0e6a8480ceaf61fc356cf
wickidcow <jaywago@gmail.com>
2026-09-21 20:16:32 -0400

    Update modern Bukkit registry names

--- src/main/java/io/github/sefiraat/crystamaehistoria/slimefun/items/mechanisms/chroniclerpanel/ChroniclerPanelCache.java
+++ src/main/java/io/github/sefiraat/crystamaehistoria/slimefun/items/mechanisms/chroniclerpanel/ChroniclerPanelCache.java
@@ -279,7 +279,7 @@ public class ChroniclerPanelCache extends AbstractCache {
         final Location location = blockMenu.getLocation();
         for (int i = 0; i < 2; i++) {
             final Location l = location.clone().add(ThreadLocalRandom.current().nextDouble(0, 1.1), 1, ThreadLocalRandom.current().nextDouble(0, 1.1));
-            location.getWorld().spawnParticle(Particle.ENCHANTMENT_TABLE, l, 0, 0.2, 0, -0.2, 0);
+            location.getWorld().spawnParticle(Particle.ENCHANT, l, 0, 0.2, 0, -0.2, 0);
         }
     }
 
