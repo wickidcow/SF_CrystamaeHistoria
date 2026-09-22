3d606181292745b0f6140ffb4dba5e5d7418bf8c
wickidcow <jaywago@gmail.com>
2026-09-22 13:07:09 -0400

    Resolve remaining 1.21.11 compatibility warnings

--- src/main/java/io/github/sefiraat/crystamaehistoria/slimefun/itemgroups/StoryCollectionFlexGroup.java
+++ src/main/java/io/github/sefiraat/crystamaehistoria/slimefun/itemgroups/StoryCollectionFlexGroup.java
@@ -37,6 +37,7 @@ import java.util.stream.Collectors;
 /**
  * @noinspection deprecation
  */
+@SuppressWarnings("deprecation")
 public class StoryCollectionFlexGroup extends FlexItemGroup {
 
     private static final int PAGE_SIZE = 36;
